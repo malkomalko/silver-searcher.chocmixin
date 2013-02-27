610a425f376c59b2e9ac9fd7d5d949d1f01bd8c7
Robert Malko <robmalko@gmail.com>
2013-02-27 00:33:19 -0500

    mostly got chocolat.sendMessage working

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
 $(window).ready(function(){
-  $("input[type='search']").focus()
+  var searchBox = $("input[type='search']")
+
+  searchBox.focus()
+
+  searchBox.keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which)
+
+    if (keycode === 13) { // enter
+      chocolat.sendMessage("search", [searchBox.val()])
+    }
+  })
 })
